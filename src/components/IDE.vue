3179081472980f436cd0df0f60bb37937e4acb09
Gabe Montague <gmontague@college.harvard.edu>
2017-08-29 22:18:40 -0400

    Actually fixed mixins

--- src/components/IDE.vue
+++ src/components/IDE.vue
@@ -1,23 +1,7 @@
 <template>
 <div class="ide">
-    <h1>{{ msg }}</h1>
+    <h3>NAND Editor</h3>
     <editor></editor>
-    <h2>Essential Links</h2>
-    <ul>
-        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-        <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-        <br>
-        <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
 </div>
 </template>
 
@@ -27,9 +11,10 @@ import Editor from 'components/Editor.vue';
 export default {
     name: 'ide',
     data: function () {
-        return {
+        // TODO: Remove
+        /*return {
             msg: 'Welcome to the NAND editor'
-        };
+        };*/
     },
     components: {
         Editor
@@ -40,11 +25,10 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-//@import "grid-settings";
+@import "../styles/grid.scss";
 @import "neat";
 
 // TODO: Real style
-
 h1, h2 {
     font-weight: normal;
 }
